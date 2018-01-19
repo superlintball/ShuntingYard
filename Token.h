1185eb7f4e39271398d4ddd98ea25cadc2f00f2b
superlintball <ravnikator@gmail.com>
2018-01-19 12:25:13 -0800

    I wanna do this :(

--- Token.h
+++ Token.h
@@ -2,36 +2,60 @@
 #define TOKENH
 
 #include <iostream>
+#include <ctype.h>
 
 using namespace std;
 
 class Token
 {
 	public:
+		//token constructor; defines properties for each type of operator
 		Token(char newToken)
 		{
 			token = newToken;
-			if(token == '+' || token == '-')
+
+			//all numbers are type 0
+			if(isdigit(token))
+			{
+				type = 0;
+			}
+			
+			//all operators are type 1
+			else if(token == '+' || token == '-')
 			{
 				precedence = 1;
-				associativity = LEFT;
-			} else if(token == 'x' || token == '/')
+				associativity = 1;
+				type = 1;
+			} else if(token == '*' || token == '/')
 			{
 				precedence = 2;
-				associativity = LEFT;
+				associativity = 1;
+				type = 1;
 			} else if(token == '^')
 			{
 				precedence = 3;
-				associativity = RIGHT;
+				associativity = 2;
+				type = 1;
+			}
+			
+			//parentheses are type 2
+			else if(token == '(' || token == ')')
+			{
+				type = 2;
 			}
 		}
 		char getToken() {return token;}
 		int getPrec() {return precedence;}
 		int getAssoc() {return associativity;}
+		int getType() {return type;}
 	private:
 		char token;
-		int precedence;
-		int associativity;
+
+		//numbers and parentheses don't have precedence or associativity, so default to 0
+		int precedence = 0;
+		int associativity = 0;
+
+		int type;
 };
 
 #endif
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Token.h"
+#include "Tree.h"
+
+using namespace std;
+
+//associativity constants
+const int NONE = 0;
+const int LEFT = 1;
+const int RIGHT = 2;
+
+//type constants
+const int NUM = 0; //number
+const int OP = 1; //operator
+const int PAR = 2; //parenthesis
+
+int main()
+{
+	return 0
+}
